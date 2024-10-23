7b759526549cbacc274e8450c2dcd206d1f750da
AverageKasper <averagekasper@gmail.com>
2024-10-23 17:03:33 +0300

    Task 2 done

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,18 @@ mod task1;
 mod task2;
 use std::io;
 use task1::hello_world;
-use task2::{area_of_circle, area_of_rectangle, hello_user, number_solution};
+use task2::{area_of_circle, area_of_rectangle, combinations, hello_user, number_solution, talent_calculation};
 fn main() {
-    println!("Python but its Rust Tasks");
+    println!("Python but its Rust");
     println!("Type the number of the task you want to execute");
     println!("Task 1 - Hello World");
     println!("Task 2.1 - Hello User");
     println!("Task 2.2 - Area of a circle");
     println!("Task 2.3 - Area of a rectange");
     println!("Task 2.4 - 3 Number sum, product and average");
+    println!("Task 2.5 - Talents, pounds and lots");
+    println!("Task 2.6 - Combination codes");
+    println!("Task  - Placeholder");
     println!("Task  - Placeholder");
     println!("Choose witch task to execute");
 
@@ -34,5 +37,9 @@ fn main() {
         area_of_rectangle();
     } else if choice == 2.4 {
         number_solution();
+    } else if choice == 2.5 {
+        talent_calculation();
+    } else if choice == 2.6 {
+        combinations();
     }
 }
--- src/task2.rs
+++ src/task2.rs
@@ -1,7 +1,6 @@
-use std::{
-    f32::consts::PI,
-    io,
-};
+use std::{f32::consts::PI, io};
+
+use rand::Rng;
 
 // float input check
 fn float_input_check(prompt: &str) -> f32 {
@@ -45,13 +44,10 @@ pub fn area_of_circle() {
 }
 // Task 2.3
 pub fn area_of_rectangle() {
-  
     let base: f32 = float_input_check("Enter the base of a rectangle.");
-    
     let height: f32 = float_input_check("Enter the height of a rectangle.");
 
     let border: f32 = base * 2.0 + height * 2.0;
-
     let area: f32 = base * height;
 
     println!(
@@ -62,19 +58,55 @@ pub fn area_of_rectangle() {
 // Task 2.4
 pub fn number_solution() {
     let num_one: f32 = float_input_check("Enter the first number");
-
     let num_two: f32 = float_input_check("Enter the second number");
-
     let num_three: f32 = float_input_check("Enter the third number");
 
     let sum: f32 = num_one + num_two + num_three;
-
     let multi: f32 = num_one * num_two * num_three;
-
     let average: f32 = sum / 3.0;
 
     println!(
         "The sum is {}, product is {} and average is {}.",
         sum, multi, average
     )
+}
+// Task 2.5
+pub fn talent_calculation() {
+    let talents: f32 = float_input_check("Enter talents");
+    let pounds: f32 = float_input_check("Enter pounds");
+    let lots: f32 = float_input_check("Enter lots");
+
+    let lot_weight: f32 = 13.3;
+    let pound_weight: f32 = lot_weight * 32.0;
+    let talent_weight: f32 = pound_weight * 20.0;
+
+    let total_weight: f32 = talents * talent_weight + pounds * pound_weight + lots * lot_weight;
+    let kilogram: f32 = total_weight / 1000.0;
+    let gram: f32 = total_weight - kilogram.floor() * 1000.0;
+
+    println!(
+        "Total weight: {} kilograms, {:.2} grams.",
+        kilogram.floor(),
+        gram
+    )
+}
+// Task 2.6
+pub fn combinations() {
+    fn math(num: i32, random: i32) -> i32 {
+         num * 10 + random
+    }
+    let mut rng = rand::thread_rng();
+    let mut comb1: i32 = 0;
+    let mut comb2: i32 = 0;
+    for _ in 1..=3 {
+        let num = math(comb1,rng.gen_range(0..=9));
+        comb1 = num;
+    }
+    for _ in 1..=4 {
+        let num = math(comb2,rng.gen_range(1..=6));
+        comb2 = num;
+    }
+    
+
+    println!("3-digit code is {} and 4 digit code is {}", comb1, comb2)
 }
